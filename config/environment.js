4eecb4a07d6447db221b7d7758ea3642c51ffab8
Joshua Hicks <joshua.hicks@cwtinteractive.com>
2016-07-01 10:12:52 -0400

    Updated modulePrefix in config/environment.js to work with github pages

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'contacts',
+    modulePrefix: 'Ember-Contacts',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
